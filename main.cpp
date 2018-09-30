a53397b34c7be206fcd871cd95072d7dfaab791a
jjchen <wiiso853@gmail.com>
2018-09-30 15:38:05 +0800

    Hello world example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+inline void myName(const std::string& name) {
+  std::cout << "My name is " << name << "\n";
+}
+
+inline void myAge(const int& age) {
+  std::cout << "I am " << age << " years old\n";
+}
+
+inline void selfIntro(const std::string& name, const int& age) {
+  myName(name);
+  myAge(age);
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::string name = "JJC";
+  int age = 27;
+  std::thread selfIntroductrionThread(selfIntro, name, age);
+
+  if (selfIntroductrionThread.joinable()) {
+    selfIntroductrionThread.join();
+  }
   return 0;
 }
